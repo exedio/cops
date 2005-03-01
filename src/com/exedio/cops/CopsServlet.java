b1742180f93d222427b1ffafc6c570b85866f3b4
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-03-01 12:31:53 +0000

    move exception reporting from CopernicaServlet to CopsServlet
    git-svn-id: https://svn.exedio.com/cope/trunk/cops@1844 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cops/CopsServlet.java
+++ src/com/exedio/cops/CopsServlet.java
@@ -2,6 +2,8 @@
 package com.exedio.cops;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,5 +30,45 @@ public abstract class CopsServlet extends HttpServlet
 
 	protected abstract void doRequest(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException;
+
+
+	public final void printException(final PrintStream out, final Exception exception)
+	{
+		exception.printStackTrace(out);
+		if(exception instanceof ServletException)
+		{
+			final Throwable rootCause =
+				((ServletException)exception).getRootCause();
+			if(rootCause!=null)
+			{
+				out.println("root cause for ServletException:");
+				rootCause.printStackTrace(out);
+			}
+			else
+			{
+				out.println("no root cause for ServletException");
+			}
+		}
+		out.flush();
+	}
+	
+	private final Random random = new Random();
+
+	/**
+	 * Returns the id under with the exception has been reported in the log.
+	 */
+	public final String reportException(final Exception exception)
+	{
+		final long idLong;
+		synchronized(random)
+		{
+			idLong = random.nextLong();
+		}
+		final String id = String.valueOf(Math.abs(idLong));
+		System.out.println("--------I"+id+"-----");
+		printException(System.out, exception);
+		System.out.println("--------O"+id+"-----");
+		return id;
+	}
 
 }
