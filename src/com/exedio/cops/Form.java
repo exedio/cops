0f785fe863c8ce9908a26ace3bafb9bafacf105f
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-04-14 13:46:19 +0000

    introduce Form.LongField
    git-svn-id: https://svn.exedio.com/cope/trunk/cops@1984 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cops/Form.java
+++ src/com/exedio/cops/Form.java
@@ -204,6 +204,33 @@ public abstract class Form
 		
 	}
 	
+	public class LongField extends TextField
+	{
+		final Long content;
+		
+		public LongField(final Object key, final String name, final boolean readOnly, final String value, final boolean hidden)
+		{
+			super(key, name, readOnly, value, hidden);
+
+			if(value.length()>0)
+			{
+				long parsed = 0;
+				try
+				{
+					parsed = Long.parseLong(value);
+				}
+				catch(NumberFormatException e)
+				{
+					error = "bad number: "+e.getMessage();
+				}
+				content = error==null ? new Long(parsed) : null;
+			}
+			else
+				content = null;
+		}
+		
+	}
+	
 	public final List getFields()
 	{
 		return Collections.unmodifiableList(fieldList);
