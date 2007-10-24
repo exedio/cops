e586617ba1067d55779596801703101a78569415
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-10-24 16:37:20 +0000

    warning for unusedPrivateMember
    git-svn-id: https://svn.exedio.com/cope/trunk/cops@8998 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cops/Base64.java
+++ src/com/exedio/cops/Base64.java
@@ -68,38 +68,6 @@ final class Base64
     private final static String PREFERRED_ENCODING = "UTF-8";
 
 
-    /** The 64 valid Base64 values. */
-    private final static byte[] ALPHABET;
-    private final static byte[] _NATIVE_ALPHABET = /* May be something funny like EBCDIC */
-    {
-        (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F', (byte)'G',
-        (byte)'H', (byte)'I', (byte)'J', (byte)'K', (byte)'L', (byte)'M', (byte)'N',
-        (byte)'O', (byte)'P', (byte)'Q', (byte)'R', (byte)'S', (byte)'T', (byte)'U',
-        (byte)'V', (byte)'W', (byte)'X', (byte)'Y', (byte)'Z',
-        (byte)'a', (byte)'b', (byte)'c', (byte)'d', (byte)'e', (byte)'f', (byte)'g',
-        (byte)'h', (byte)'i', (byte)'j', (byte)'k', (byte)'l', (byte)'m', (byte)'n',
-        (byte)'o', (byte)'p', (byte)'q', (byte)'r', (byte)'s', (byte)'t', (byte)'u',
-        (byte)'v', (byte)'w', (byte)'x', (byte)'y', (byte)'z',
-        (byte)'0', (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5',
-        (byte)'6', (byte)'7', (byte)'8', (byte)'9', (byte)'+', (byte)'/'
-    };
-
-    /** Determine which ALPHABET to use. */
-    static
-    {
-        byte[] __bytes;
-        try
-        {
-            __bytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".getBytes( PREFERRED_ENCODING );
-        }   // end try
-        catch (java.io.UnsupportedEncodingException use)
-        {
-            __bytes = _NATIVE_ALPHABET; // Fall back to native encoding
-        }   // end catch
-        ALPHABET = __bytes;
-    }   // end static
-
-
     /**
      * Translates a Base64 value to either its 6-bit reconstruction value
      * or a negative number indicating some other meaning.
