exedio/cops

commit 3869b41db6a996388ea76411b4e2863a6d4c5fc6

remove the need for setPath
git-svn-id: https://svn.exedio.com/cope/trunk/cops@11125 e7d4fc99-c606-0410-b9bf-843393a9eab7

src/com/exedio/cops/CopsServlet.java
@@ -48,8 +48,6 @@ public abstract class CopsServlet extends HttpServlet
 	
 	private final LinkedHashMap<String, Resource> resources;
 	
-	private boolean contextPathOnResourcesToBeSet = false;
-	
 	protected CopsServlet()
 	{
 		try
@@ -70,7 +68,6 @@ public abstract class CopsServlet extends HttpServlet
 				
 				resource.init(clazz);
 				resources.put('/'+resource.name, resource);
-				contextPathOnResourcesToBeSet = true;
 			}
 			this.resources = resources.isEmpty() ? null : resources;
 		}
@@ -98,13 +95,6 @@ public abstract class CopsServlet extends HttpServlet
 		
 		if(resources!=null)
 		{
-			if(contextPathOnResourcesToBeSet)
-			{
-				contextPathOnResourcesToBeSet = false;
-				final String path = request.getContextPath() + request.getServletPath() + '/';
-				for(final Resource resource : resources.values())
-					resource.setPath(path);
-			}
 			if("/copsResourceStatus.html".equals(pathInfo))
 			{
 				if(!request.isUserInRole("manager"))

src/com/exedio/cops/Resource.java
@@ -39,7 +39,6 @@ public final class Resource
 
 	private final Object contentLock = new Object();
 	private byte[] content;
-	private String toString;
 	
 	private volatile long response200Count = 0;
 	private volatile long response304Count = 0;
@@ -94,7 +93,10 @@ public final class Resource
 	@Override
 	public String toString()
 	{
-		return toString;
+		final HttpServletRequest request = CopsServlet.requests.get();
+		if(request==null)
+			throw new IllegalStateException("no request available");
+		return request.getContextPath() + request.getServletPath() + '/' + name;
 	}
 	
 	public String toAbsolute()
@@ -105,12 +107,9 @@ public final class Resource
 		return
 			request.getScheme() + "://" +
 			request.getHeader("Host") +
-			toString;
-	}
-	
-	void setPath(final String path)
-	{
-		toString = path + name;
+			request.getContextPath() +
+			request.getServletPath() +
+			'/' + name;
 	}
 	
 	void init(final Class<?> resourceLoader)

testsrc/com/exedio/cops/ResourceTest.java
@@ -37,7 +37,15 @@ public class ResourceTest extends TestCase
 		assertEquals(-1, r1.getContentLength());
 		assertEquals(0, r1.getResponse200Count());
 		assertEquals(0, r1.getResponse304Count());
-		assertEquals(null, r1.toString());
+		try
+		{
+			r1.toString();
+			fail();
+		}
+		catch(IllegalStateException e)
+		{
+			assertEquals("no request available", e.getMessage());
+		}
 		try
 		{
 			r1.toAbsolute();
@@ -57,7 +65,15 @@ public class ResourceTest extends TestCase
 		assertTrue(String.valueOf(contentLength), contentLength>5);
 		assertEquals(0, r1.getResponse200Count());
 		assertEquals(0, r1.getResponse304Count());
-		assertEquals(null, r1.toString());
+		try
+		{
+			r1.toString();
+			fail();
+		}
+		catch(IllegalStateException e)
+		{
+			assertEquals("no request available", e.getMessage());
+		}
 		try
 		{
 			r1.toAbsolute();
@@ -77,26 +93,15 @@ public class ResourceTest extends TestCase
 		assertEquals(contentLength, r1.getContentLength());
 		assertEquals(0, r1.getResponse200Count());
 		assertEquals(0, r1.getResponse304Count());
-		assertEquals(null, r1.toString());
 		try
 		{
-			r1.toAbsolute();
+			r1.toString();
 			fail();
 		}
 		catch(IllegalStateException e)
 		{
 			assertEquals("no request available", e.getMessage());
 		}
-		
-		
-		r1.setPath("/contextPath/servletPath/");
-		assertEquals("ResourceTest.class", r1.getName());
-		assertEquals("major/minor", r1.getContentType());
-		assertEquals(lastModified, r1.getLastModified());
-		assertEquals(contentLength, r1.getContentLength());
-		assertEquals(0, r1.getResponse200Count());
-		assertEquals(0, r1.getResponse304Count());
-		assertEquals("/contextPath/servletPath/ResourceTest.class", r1.toString());
 		try
 		{
 			r1.toAbsolute();
@@ -110,6 +115,8 @@ public class ResourceTest extends TestCase
 		
 		CopsServlet.requests.set(new DummyRequest(){
 			@Override public String getScheme(){return "scheme";}
+			@Override public String getContextPath(){return "/contextPath";}
+			@Override public String getServletPath(){return "/servletPath";}
 			@Override public String getHeader(String name)
 			{
 				if("Host".equals(name))

commit 4ec4a19bef005efef9ce99080c501a206eba11b4

fix copsResourceStatus, was broken in r11125: remove the need for setPath
git-svn-id: https://svn.exedio.com/cope/trunk/cops@11136 e7d4fc99-c606-0410-b9bf-843393a9eab7

src/com/exedio/cops/CopsServlet.java
@@ -111,6 +111,10 @@ public abstract class CopsServlet extends HttpServlet
 				nfs.setGroupingSeparator('\'');
 				final DecimalFormat nf = new DecimalFormat("", nfs);
 				final PrintStream out = new PrintStream(response.getOutputStream(), false, ENCODING);
+				try
+				{
+					assert requests.get()==null;
+					requests.set(request);
 				ResourceStatus_Jspm.write(
 						out,
 						getServletConfig().getServletName(),
@@ -121,6 +125,11 @@ public abstract class CopsServlet extends HttpServlet
 						new SimpleDateFormat("yyyy/MM/dd'&nbsp;'HH:mm:ss'<small>'.S'</small>'"),
 						new SimpleDateFormat("yyyy/MM/dd'&nbsp;'HH:mm:ss.SSS Z (z)"),
 						CopsServlet.class.getPackage());
+				}
+				finally
+				{
+					requests.remove();
+				}
 				out.close();
 				return;
 			}