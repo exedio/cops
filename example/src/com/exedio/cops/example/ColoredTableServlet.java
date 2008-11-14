26c76df533a13ec3b5b22d470dbcdb0bc39d7089
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-11-14 10:14:36 +0000

    forgot to add in r11340: add ColoredTable
    git-svn-id: https://svn.exedio.com/cope/trunk/cops@11341 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ example/src/com/exedio/cops/example/ColoredTableServlet.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2004-2008  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cops.example;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.exedio.cops.CopsServlet;
+
+public final class ColoredTableServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1l;
+
+	@Override
+	protected final void doGet(
+			final HttpServletRequest request,
+			final HttpServletResponse response)
+		throws IOException
+	{
+			final PrintStream out = new PrintStream(response.getOutputStream(), false, CopsServlet.ENCODING);
+			ColoredTableServlet_Jspm.write(out);
+			out.close();
+	}
+}
