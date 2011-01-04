043a8b6318b174867694df58870ad1c24bec53c1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-01-04 14:41:34 +0000

    reuse code
    git-svn-id: https://svn.exedio.com/cope/cops/trunk@18670 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cops/Resource.java
+++ src/com/exedio/cops/Resource.java
@@ -66,7 +66,7 @@ public final class Resource
 
 		this.name = name;
 		this.contentType = contentType;
-		this.lastModified = ((System.currentTimeMillis()+999)/1000)*1000;
+		this.lastModified = roundLastModified(System.currentTimeMillis());
 	}
 
 	public String getName()
@@ -89,7 +89,12 @@ public final class Resource
 		if(lastModified<0)
 			throw new IllegalArgumentException("lastModified must not be negative, but was " + lastModified);
 
-		this.lastModified = ((lastModified+999)/1000)*1000;
+		this.lastModified = roundLastModified(lastModified);
+	}
+
+	private static long roundLastModified(final long lastModified)
+	{
+		return ((lastModified+999)/1000)*1000;
 	}
 
 	public int getContentLength()
