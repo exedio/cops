361dfd550015d1620f76380f8119f15471ddb997
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-07-29 11:31:13 +0000

    revert r26341: svn merge -r26341:r26340 .
    git-svn-id: https://svn.exedio.com/cope/cops/trunk@26342 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- makeSourceZip.sh
+++ makeSourceZip.sh
@@ -1,14 +1,12 @@
 DIR=`dirname ${0}`
 cd ${DIR}
-mkdir --parents build
 svn \
 	list \
 	--recursive \
 	. \
 	> build/makeSourceZipFiles.txt
-rm -f build/success/exedio-cops-src-build*.tar.gz
-mkdir --parents build/success
+rm -f build/exedio-cops-src-build*.tar.gz
 tar czf \
-	build/success/exedio-cops-src-build${BUILD_NUMBER}.tar.gz \
+	build/exedio-cops-src-build${BUILD_NUMBER}.tar.gz \
 	--files-from=build/makeSourceZipFiles.txt \
 	--no-recursion
