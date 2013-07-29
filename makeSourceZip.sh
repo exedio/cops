71db413bcd19b74e9b256d74316a8a3fd449ca6b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-07-29 11:28:07 +0000

    put source zip into build/success
    git-svn-id: https://svn.exedio.com/cope/cops/trunk@26341 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- makeSourceZip.sh
+++ makeSourceZip.sh
@@ -1,12 +1,14 @@
 DIR=`dirname ${0}`
 cd ${DIR}
+mkdir --parents build
 svn \
 	list \
 	--recursive \
 	. \
 	> build/makeSourceZipFiles.txt
-rm -f build/exedio-cops-src-build*.tar.gz
+rm -f build/success/exedio-cops-src-build*.tar.gz
+mkdir --parents build/success
 tar czf \
-	build/exedio-cops-src-build${BUILD_NUMBER}.tar.gz \
+	build/success/exedio-cops-src-build${BUILD_NUMBER}.tar.gz \
 	--files-from=build/makeSourceZipFiles.txt \
 	--no-recursion
