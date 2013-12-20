443829955cafcf6a1a718e4e662be9dac7bbd0e2
Frank Finger <frank.finger@exedio.com>
2013-12-20 08:56:01 +0000

    added option to un/check all tests to properties screen
    git-svn-id: https://svn.exedio.com/cope/cops/trunk@27323 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cops/properties.js
+++ src/com/exedio/cops/properties.js
@@ -29,3 +29,15 @@ function toggleUnspecified(image)
 		}
 	}
 }
+
+function checkAll(bx) 
+{
+  var cbs = document.getElementsByTagName('input');
+  for(var i=0; i < cbs.length; i++) 
+  {
+    if(cbs[i].type=='checkbox' && cbs[i].className=='testcheck')
+	 {
+      cbs[i].checked = bx.checked;
+    }
+  }
+}
